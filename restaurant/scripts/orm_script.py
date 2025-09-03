9acb83ec1dde176373fcef91de731df6367cd661
Abhishek Ojha <aojha6822@gmail.com>
2025-09-03 02:26:45 -0700

    db populated

--- restaurant/scripts/orm_script.py
+++ restaurant/scripts/orm_script.py
@@ -15,4 +15,48 @@ def run():
     # ratings = restaurant.ratings.all()
     # print(ratings)
 
+    # user = User.objects.first()
+    # restaurant = Restaurant.objects.first()
+
+    # rating, created = Rating.objects.get_or_create(
+    #     user=user, restaurant=restaurant, rating=9
+    # )
+    # if created:
+    #     print("Rating is created")
+    # else:
+    #     print(rating)
+
+    # rating = Rating(user=user, restaurant=restaurant, rating=10)
+    # rating.full_clean()
+    # rating.save()
+
+    # restaurant = Restaurant.objects.last()
+    # restaurant.name = "Ram lal ko paan pasal"
+    # restaurant.save(update_fields=["name"])
+
+    # Restaurant.objects.create(
+    #     name="Aurora Cafe",
+    #     website="https://auroracafe.com",
+    #     date_opened="2021-5-15",
+    #     latitude=40.7128,
+    #     longitude=-74.0060,
+    #     restaurant_type=Restaurant.TypeChoices.MEXICAN,  # must match one of the defined choices
+    # )
+    # print(restaurant)
+
+    # restaurants = Restaurant.objects.all()
+    # restaurants.update(date_opened=timezone.now())
+
+    # restaurants = Restaurant.objects.filter(name__startswith="shyam")
+    # print(restaurants.update(name="Shyam lal ko paan pasal"))
+    # print(restaurants)
+
+    # restaurant = Restaurant.objects.first()
+    # print(restaurant.pk)
+    # print(restaurant.ratings.all())
+    # restaurant.delete()
+
+    restaurant = Restaurant.objects.all().delete()
+    print(restaurant)
+
     pprint(connection.queries)
