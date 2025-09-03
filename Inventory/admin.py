ce1fc915d7b158b294ce78476f2f7fe34ebbd97d
Abhishek Ojha <aojha6822@gmail.com>
2025-09-02 21:39:06 -0700

    restaurant app created

--- Inventory/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product, Stock, Brand, Category
-
-admin.site.register(Product)
-admin.site.register(Stock)
-admin.site.register(Brand)
-admin.site.register(Category)
